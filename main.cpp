7dfd522dbc664bd18b5af4bc77ac21776de3723b
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-10-26 19:11:29 +0200

    Finish candy project

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+int uczniowie, cukierki,x,y;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Ilu uczniow jest w Twojej klasie:";
+    cin >> uczniowie;
+    cout << "Ile cukierkow kupila mama:";
+    cin>>cukierki;
+    x = cukierki/(uczniowie-1);
+    cout<<"Cukierkow dla kazdego ucznia:"<< x;
+    y = cukierki-x*(uczniowie-1);
+    cout<<endl<<"Dla Jasia na wieczor: "<<y;
     return 0;
 }
